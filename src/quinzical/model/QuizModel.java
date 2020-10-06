914bdb217393637f4fe50f1121155402f32b0acf
dcut417 <dcut417@aucklanduni.ac.nz>
2020-10-06 14:43:18 +1300

    Update QuizModel.java
    Updated validate() method to deal with answers beginnning with "a" or "the" and questions with multiple answers

--- src/quinzical/model/QuizModel.java
+++ src/quinzical/model/QuizModel.java
@@ -130,7 +130,25 @@ public class QuizModel {
 				return true;
 			} else if (i == 0) {
 				answer = replaceMacrons(answer);
-				i = 1;
+				i++;
+			} else if (i == 1) {
+				userInput = userInput.toLowerCase();
+				if (userInput.startsWith("the ")) {
+					userInput = userInput.substring(4);
+				} else if (userInput.startsWith("a ")) {
+					userInput = userInput.substring(2);
+				}
+				i++;
+			} else if (i == 2) {
+				if (answer.contains("/")) {
+					String[] answers = answer.split("/");
+					for (int j = 0; j < answers.length; j++) {
+						if (userInput.equalsIgnoreCase(answers[j]) || userInput.equalsIgnoreCase(prefix + " " + answers[j])) {
+							return true;
+						}
+					}
+				}
+				i++;
 			} else {
 				return false;
 			}
